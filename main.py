ac17d3b1c11e27f121f70ab0bbe7262bcb56f6db
unknown <myusernamethis@outlook.de>
2023-01-25 20:04:31 +0100

    upgrade

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 soln = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 gutel = [0,1,2,3,4,5,6,7,8,9]
 zlist = [" "," "," "," "," "," "," "," "," "," "]
-winl2 = []
-winl1 = []
+winl = [[],[]]
+
 # Function to print Tic Tac Toe
 def print_tic_tac_toe(wert):
     print("\n")
@@ -34,33 +34,32 @@ def eingabe(x,b):
 
 while True:
     for x in soln:
-        if winl1 == x:
+        if winl[0] == x:
             print("2 gewinnt")
             exit()
-        if winl2 == x:
+        if winl[1] == x:
             print("1 gewinnt")
             exit()
-        if len(winl1) > 4:
+        if len(winl[0]) > 4:
             print("unentschieden")
             exit()
-        if len(winl2) > 4:
+        if len(winl[1]) > 4:
             print("unentschieden")
             exit()
-    if len(winl1) < len(winl2):
-        print("Spieler 2")
-        y = int(input("Wahl: "))
-        winl1.append(y)
-        eingabe(y,"o")
-        print_tic_tac_toe(gutel)
 
-
-
+    if len(winl[0]) < len(winl[1]):
+        t = "o"
+        p = "2"
+        z = 0
     else:
-        print("Spieler 1")
-        y = int(input("Wahl: "))
-        winl2.append(y)
-        eingabe(y,"x")
-        print_tic_tac_toe(gutel)
+        t = "x"
+        p = "1"
+        z = 1
 
+    print("Spieler {}".format(p))
+    y = int(input("Wahl: "))
+    winl[z].append(y)
+    eingabe(y, t)
+    print_tic_tac_toe(gutel)
 
 
